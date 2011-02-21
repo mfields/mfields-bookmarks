a7c293d6866f62590fd4f355f372cfb2ed54eec1
mfields <michael@mfields.org>
2011-02-20 18:19:47 -0800

    Added Docs, fine tune custom objects, moved hooks inside constructor.

--- mfields-bookmarks.php
+++ mfields-bookmarks.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name:       Mfields Bookmarks
+Plugin Name:       AAA Mfields Bookmarks
 Description:       Enables a custom post_type for bookmarks.
-Version:           0.2
+Version:           0.3
 Author:            Michael Fields
 Author URI:        http://wordpress.mfields.org/
 License:           GPLv2 or later
@@ -23,6 +23,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 Changelog
+0.3
+	- Added Documentation
+	- Fine tuning of custom objects.
+	- Move all hooks inside constructor.
 0.2
 	- Flush rewrite on activation and deactivation.
 	- Created Mfields_Bookmark_Post_Type class.
@@ -30,32 +34,68 @@ Changelog
 	- Initial plugin
 */
 
-
 class Mfields_Bookmark_Post_Type {
 	/**
 	 * Constructor.
+	 *
+	 * Hook into WordPress.
+	 *
+	 * @return     void
+	 * @since      2011-02-20
 	 */
 	 function Mfields_Bookmark_Post_Type() {
+		register_activation_hook( __FILE__, array( &$this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
 		add_action( 'init', array( &$this, 'register_post_type' ), 0 );
-		add_action( 'init', array( &$this, 'register_taxonomies' ), 1 );
+		add_action( 'init', array( &$this, 'register_taxonomies' ), 0 );
 		add_action( 'admin_head-post-new.php', array( &$this, 'process_bookmarklet' ) );
 		add_filter( 'the_content', array( &$this, 'append_link_to_content' ), 0 );
 	}
 	/**
 	 * Activation.
+	 *
+	 * When a user activates this plugin the public pages
+	 * for both custom taxonomies and post_types will need
+	 * to be immediately available. To ensure that this happens
+	 * both post_types and taxonomies need to be registered at
+	 * activation so that their rewrite rules will be present
+	 * when new rules are added to the database during flush.
+	 *
+	 * @return     void
+	 * @since      2011-02-20
 	 */
 	function activate() {
 		$this->register_post_type();
+		$this->register_taxonomies();
 		flush_rewrite_rules();
 	}
 	/**
 	 * Deactivation.
+	 *
+	 * When a user chooses to deactivate bookmarks it is
+	 * important to remove all custom object rewrites from
+	 * the database.
+	 *
+	 * @return     void
+	 * @since      2011-02-20
 	 */
 	function deactivate() {
 		flush_rewrite_rules();
 	}
 	/**
 	 * Register post_type.
+	 *
+	 * Registers custom post_type 'mfields_bookmark' with
+	 * WordPress.
+	 *
+	 * This method is hooked into the init action and should
+	 * fire everywhere save the deactivation hook. When this
+	 * plugin is deactivated this method will return early.
+	 * This makes it easy for the deactivation() method to do
+	 * its job.
+	 *
+	 * @return     void
+	 * @since      2011-02-20
 	 */
 	function register_post_type() {
 		if ( isset( $_REQUEST['action'] ) && 'deactivate' == $_REQUEST['action'] ) {
@@ -93,12 +133,26 @@ class Mfields_Bookmark_Post_Type {
 	}
 	/**
 	 * Register taxonomies.
+	 *
+	 * Register all taxonomy's for bookmarks including:
+	 * "Type", "Source", and "Topic".
+	 *
+	 * This function will look for an already installed taxonomy
+	 * named "topic" and, if found, will register it with the
+	 * 'mfields_bookmark' object type. On installations not having
+	 * a global "topic" taxonomy, a new one will be registered.
+	 *
+	 * @return     void
+	 * @since      2011-02-20
 	 */
 	function register_taxonomies() {
+		if ( isset( $_REQUEST['action'] ) && 'deactivate' == $_REQUEST['action'] ) {
+			return;
+		}
 		register_taxonomy( 'mfields_bookmark_type', 'mfields_bookmark', array(
 			'hierarchical'          => true,
 			'query_var'             => 'bookmark_type',
-			'rewrite'               => array( 'slug' => 'bookmark_type' ),
+			'rewrite'               => array( 'slug' => 'bookmark-type' ),
 			'show_tagcloud'         => false,
 			'update_count_callback' => '_update_post_term_count',
 			'labels' => array(
@@ -116,9 +170,9 @@ class Mfields_Bookmark_Post_Type {
 			) );
 		register_taxonomy( 'mfields_bookmark_source', 'mfields_bookmark', array(
 			'hierarchical'          => true,
-			'query_var'             => 'source',
-			'rewrite'               => array( 'slug' => 'source' ),
-			'show_tagcloud'         => true,
+			'query_var'             => 'bookmark_source',
+			'rewrite'               => array( 'slug' => 'bookmark-source' ),
+			'show_tagcloud'         => false,
 			'update_count_callback' => '_update_post_term_count',
 			'labels' => array(
 				'name'              => 'Sources',
@@ -133,7 +187,30 @@ class Mfields_Bookmark_Post_Type {
 				'new_item_name'     => 'New Source'
 				)
 			) );
-		register_taxonomy_for_object_type( 'topics', 'mfields_bookmark' );
+		if ( taxonomy_exists( 'topics' ) ) {
+			register_taxonomy_for_object_type( 'topics', 'mfields_bookmark' );
+		}
+		else {
+			register_taxonomy( "mfields_bookmark_topic", 'post', array(
+				'hierarchical'          => true,
+				'query_var'             => 'bookmark_topic',
+				'rewrite'               => array( 'slug' => 'bookmark-topic' ),
+				'show_tagcloud'         => false,
+				'update_count_callback' => '_update_post_term_count',
+				'labels' => array(
+					'name'              => 'Topics',
+					'singular_name'     => 'Topic',
+					'search_items'      => 'Search Topics',
+					'all_items'         => 'All Topics',
+					'parent_item'       => 'Parent Topic',
+					'parent_item_colon' => 'Parent Topic:',
+					'edit_item'         => 'Edit Topic',
+					'update_item'       => 'Update Topic',
+					'add_new_item'      => 'Add a New Topic',
+					'new_item_name'     => 'New Topic Name'
+					)
+				) );
+		}
 	}
 	/**
 	 * Append Bookmark link to the content on multiple views.
@@ -165,6 +242,3 @@ EOF;
 	}
 }
 $mfields_bookmark_post_type = new Mfields_Bookmark_Post_Type();
-
-register_activation_hook( __FILE__, array( &$mfields_bookmark_post_type, 'activate' ) );
-register_deactivation_hook( __FILE__, array( &$mfields_bookmark_post_type, 'deactivate' ) );
